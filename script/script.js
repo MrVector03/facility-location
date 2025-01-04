4d299a00fd4036ae56b5905af078efa7cc5c6d0c
Viktor <viktormilojevic.03@gmail.com>
2025-01-04 02:06:33 +0100

    Add manual functions

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
-console.log(Konva);
+Konva.showWarnings = false;
 
 const MAX_POINTS = 500;
 
-const infoText = "Covered points: ";
+const infoText = "Maximum points covered: ";
+const manualInfoText = "Manually covered points: ";
 
 const stage = new Konva.Stage({
     container: 'simulator-stage',
@@ -17,7 +18,24 @@ const border = new Konva.Rect({
     height: stage.height(),
     stroke: 'black',
     strokeWidth: 2,
-    listening: false,
+})
+
+const selectionRectangle = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    fill: 'rgba(255, 0, 0, 0.3)',
+    visible: false,
+});
+
+const manualDragDisc = new Konva.Circle({
+    x: 0,
+    y: 0,
+    radius: 0,
+    fill: 'rgba(0, 0, 255, 0.3)',
+    visible: false,
+
 })
 
 class Painters {
@@ -27,7 +45,6 @@ class Painters {
     discRadius = 50;
     discX = -100;
     discY = -100;
-    manualDisc = null; /// TODO: For manual drag-and-dropping over the points
 
     masterCount = 0;
 
@@ -54,10 +71,9 @@ class Painters {
         //layer.add(this.finalDisc);
         layer.add(border);
 
-        document.getElementById("score").innerHTML = infoText + master.masterCount;
+        document.getElementById("score").innerHTML = infoText + '?';
 
         layer.draw();
-        console.log("MASTER DRAW");
     }
 
     drawTemporary(x, y) {
@@ -110,6 +126,12 @@ function customCompare(A, B) {
         return A.second === 1 ? -1 : 1;
 }
 
+function isPointInsideManualDisc(point) {
+    const dx = point.x - manualDragDisc.x();
+    const dy = point.y - manualDragDisc.y();
+    return (dx * dx + dy * dy) <= (manualDragDisc.radius() * manualDragDisc.radius());
+}
+
 let master = new Painters();
 
 const layer = new Konva.Layer();
@@ -141,7 +163,7 @@ function scatterPoints(numberOfPoints) {
     });
 
     master.masterCount = 0;
-    document.getElementById("score").innerHTML = infoText + master.masterCount;
+    document.getElementById("score").innerHTML = infoText + '?';
     master.masterDraw();
 }
 
@@ -227,7 +249,102 @@ document.getElementById("clear-points").addEventListener('click', () => {
     master.points = [];
     master.masterCount = 0;
     master.masterDraw();
-    document.getElementById("score").innerHTML = infoText + master.masterCount;
+    document.getElementById("score").innerHTML = infoText + '?';
 })
+
+stage.on('click', function () {
+    if (document.getElementById("insert-points-manual").checked) {
+        const {x, y} = stage.getPointerPosition();
+        master.points.push(new Point(x, y));
+        master.masterDraw();
+    }
+});
+
+let x1, y1, x2, y2;
+let removeManual = document.getElementById('remove-points-manual');
+
+stage.on('mousedown', (position) => {
+    if (position.target === stage && removeManual.checked) {
+        x1 = stage.getPointerPosition().x;
+        y1 = stage.getPointerPosition().y;
+
+        selectionRectangle.visible(true);
+
+        selectionRectangle.width(0);
+        selectionRectangle.height(0);
+        selectionRectangle.position({x: x1, y: y1});
+
+        layer.add(selectionRectangle);
+        layer.draw();
+    }
+});
+
+
+stage.on('mousemove', (e) => {
+    let dragManual = document.getElementById('drag-disc-manual');
+
+    if (dragManual.checked) {
+        const {x, y} = stage.getPointerPosition();
+
+        manualDragDisc.setAttrs({
+            x: x,
+            y: y,
+            radius: master.discRadius,
+        })
+        manualDragDisc.visible(true);
+
+        let count = 0;
+        master.points.forEach((point) => {
+            if (isPointInsideManualDisc(point))
+                count++;
+        });
+
+        document.getElementById('manual-score').innerHTML = manualInfoText + count;
+
+        layer.add(manualDragDisc);
+        layer.batchDraw();
+    } else {
+
+        manualDragDisc.visible(false);
+
+        if (!selectionRectangle.visible() || !removeManual.checked) return;
+
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
+
+        selectionRectangle.setAttrs({
+            x: Math.min(x1, x2),
+            y: Math.min(y1, y2),
+            width: Math.abs(x2 - x1),
+            height: Math.abs(y2 - y1),
+        });
+
+        layer.batchDraw();
+    }
+});
+
+stage.on('mouseup', () => {
+    if (!selectionRectangle.visible() || !removeManual.checked) return;
+
+    const selectionBox = selectionRectangle.getClientRect();
+
+    master.points = master.points.filter((point) => {
+        const isInside =
+            point.x >= selectionBox.x &&
+            point.x <= selectionBox.x + selectionBox.width &&
+            point.y >= selectionBox.y &&
+            point.y <= selectionBox.y + selectionBox.height;
+
+        master.masterCount = 0;
+
+        document.getElementById('score').innerHTML = infoText + '?';
+
+        return !isInside;
+    });
+
+    selectionRectangle.visible(false);
+    master.masterDraw();
+});
+
 
 master.masterDraw();
